107748f1e6768ea4b562f3999ac8734f5a85707e
Mast3rone <100630078+Mast3rone@users.noreply.github.com>
2026-03-24 20:09:37 +0100

    asd

--- next.config.js
+++ next.config.js
@@ -3,11 +3,17 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  // Note: This feature is required to use NextJS Image in SSG mode.
-  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
 module.exports = nextConfig;
+
+/* nigeria */ 
